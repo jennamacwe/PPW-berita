408fb80dfce24a4df29a1ea472406ca2121ef336
jennamacwe <107898237+jennamacwe@users.noreply.github.com>
2024-10-02 23:06:32 +0700

    Update berita.py

--- berita.py
+++ berita.py
@@ -735,7 +735,7 @@ st.write("Nama  : Jennatul Macwe ")
 st.write("Nim   : 210411100151 ")
 st.write("Kelas : Pencarian dan Penambangan WEB B ")
 
-data_set_description, dataset, prepro, model, Input = st.tabs(["Deskripsi Data Set", "Dataset", "Pre-processing", "Model", "Klasifikasi"])
+data_set_description, dataset, prepro, model, Input = st.tabs(["Deskripsi Data Set", "Dataset", "Pre-processing", "Klasifikasi"])
 
 with data_set_description:
     st.write("### Deskripsi Dataset")
