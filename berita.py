1ef9b1ff3713f33e9d25c09d6ebeae1467412792
jennamacwe <107898237+jennamacwe@users.noreply.github.com>
2024-10-02 23:59:08 +0700

    Update berita.py

--- berita.py
+++ berita.py
@@ -45,7 +45,7 @@ with dataset:
     st.write("### Dataset Berita Times Indonesia Kategori Politik dan Gaya Hidup")
 
     # Membaca data dari URL
-    url = "https://raw.githubusercontent.com/jennamacwe/PPW-berita/refs/heads/main/Tugas-Crawling-Data-Berita-2-kategori.csv"
+    url = "https://raw.githubusercontent.com/jennamacwe/PPW-berita/refs/heads/main/Tugas-Crawling-Data-Berita-2-kategori(1).csv"
     data = pd.read_csv(url, header=None)
 
     # Menampilkan data
@@ -58,284 +58,284 @@ with dataset:
     st.write("* Tanggal Berita : Fitur ini menunjukkan kapan berita tersebut diterbitkan berupa tanggal dan waktu")
     st.write("* Kategori Berita : Label yang menunjukkan apakah berita terkait Politik atau Gaya Hidup.")
 
-with prepro:
-    st.write("### Preprocessing Dataset")
+# with prepro:
+#     st.write("### Preprocessing Dataset")
 
-    data['Kategori'] = data[4].map({'Politik': 0, 'Gaya Hidup': 1})
+#     data['Kategori'] = data[4].map({'Politik': 0, 'Gaya Hidup': 1})
 
-    st.write("***1. Cleansing***")
+#     st.write("***1. Cleansing***")
 
-    def remove_url(data_berita):
-        url = re.compile(r'https?://\S+|www\.S+')
-        return url.sub(r'', data_berita)
+#     def remove_url(data_berita):
+#         url = re.compile(r'https?://\S+|www\.S+')
+#         return url.sub(r'', data_berita)
 
-    def remove_html(data_berita):
-        html = re.compile(r'<.#?>')
-        return html.sub(r'', data_berita)
+#     def remove_html(data_berita):
+#         html = re.compile(r'<.#?>')
+#         return html.sub(r'', data_berita)
 
-    def remove_emoji(data_berita):
-        emoji_pattern = re.compile("[" 
-                                   u"\U0001F600-\U0001F64F" 
-                                   u"\U0001F300-\U0001F5FF"  
-                                   u"\U0001F680-\U0001F6FF"  
-                                   u"\U0001F1E0-\U0001F1FF" 
-                                   "]+", flags=re.UNICODE)
-        return emoji_pattern.sub(r'', data_berita)
+#     def remove_emoji(data_berita):
+#         emoji_pattern = re.compile("[" 
+#                                    u"\U0001F600-\U0001F64F" 
+#                                    u"\U0001F300-\U0001F5FF"  
+#                                    u"\U0001F680-\U0001F6FF"  
+#                                    u"\U0001F1E0-\U0001F1FF" 
+#                                    "]+", flags=re.UNICODE)
+#         return emoji_pattern.sub(r'', data_berita)
 
-    def remove_numbers(data_berita):
-        return re.sub(r'\d+', '', data_berita)
+#     def remove_numbers(data_berita):
+#         return re.sub(r'\d+', '', data_berita)
 
-    def remove_symbols(data_berita):
-        return re.sub(r'[^a-zA-Z0-9\s]', '', data_berita)
+#     def remove_symbols(data_berita):
+#         return re.sub(r'[^a-zA-Z0-9\s]', '', data_berita)
     
 
-    data['cleansing'] = data[2].apply(lambda x: remove_url(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_html(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_emoji(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_symbols(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_numbers(x))
+#     data['cleansing'] = data[2].apply(lambda x: remove_url(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_html(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_emoji(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_symbols(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_numbers(x))
 
-    # st.write("CLEANSING")
+#     # st.write("CLEANSING")
 
-    st.dataframe(data['cleansing'])
+#     st.dataframe(data['cleansing'])
 
-    st.write("***2. Case folding***")
+#     st.write("***2. Case folding***")
 
-    def case_folding(text):
-        if isinstance(text, str):
-            lowercase_text = text.lower()
-            return lowercase_text
-        else :
-            return text
+#     def case_folding(text):
+#         if isinstance(text, str):
+#             lowercase_text = text.lower()
+#             return lowercase_text
+#         else :
+#             return text
         
-    data['case_folding'] = data['cleansing'].apply(case_folding)
+#     data['case_folding'] = data['cleansing'].apply(case_folding)
 
-    st.dataframe(data['case_folding'])
+#     st.dataframe(data['case_folding'])
 
-    st.write("***3. Tokenization***")
+#     st.write("***3. Tokenization***")
 
-    def tokenize(text):
-        tokens = text.split()
-        return tokens
+#     def tokenize(text):
+#         tokens = text.split()
+#         return tokens
 
-    data['tokenize'] = data['case_folding'].apply(tokenize)
+#     data['tokenize'] = data['case_folding'].apply(tokenize)
 
-    st.dataframe(data['tokenize'])
+#     st.dataframe(data['tokenize'])
 
 
-    st.write("***3. Stop Words***")
+#     st.write("***3. Stop Words***")
 
-    # Download stopwords (pastikan hanya perlu sekali saja)
-    nltk.download('stopwords')
+#     # Download stopwords (pastikan hanya perlu sekali saja)
+#     nltk.download('stopwords')
 
-    # Mengambil daftar stopwords dalam bahasa Indonesia
-    stop_words = stopwords.words('indonesian')
+#     # Mengambil daftar stopwords dalam bahasa Indonesia
+#     stop_words = stopwords.words('indonesian')
 
-    # Fungsi untuk menghapus stopwords
-    def remove_stopwords(text):
-        return [word for word in text if word not in stop_words]
+#     # Fungsi untuk menghapus stopwords
+#     def remove_stopwords(text):
+#         return [word for word in text if word not in stop_words]
 
-    # Asumsi 'data' adalah dataframe, dengan kolom 'tokenize' yang berisi teks yang sudah di-tokenisasi
-    data['stopword_removal'] = data['tokenize'].apply(lambda x: ' '.join(remove_stopwords(x)))
+#     # Asumsi 'data' adalah dataframe, dengan kolom 'tokenize' yang berisi teks yang sudah di-tokenisasi
+#     data['stopword_removal'] = data['tokenize'].apply(lambda x: ' '.join(remove_stopwords(x)))
 
-    # Menampilkan dataframe setelah penghapusan stopwords
-    st.dataframe(data['stopword_removal'])
+#     # Menampilkan dataframe setelah penghapusan stopwords
+#     st.dataframe(data['stopword_removal'])
 
-with prepro:
-    st.write("### Preprocessing Dataset")
+# with prepro:
+#     st.write("### Preprocessing Dataset")
 
-    data['Kategori'] = data[4].map({'Politik': 0, 'Gaya Hidup': 1})
+#     data['Kategori'] = data[4].map({'Politik': 0, 'Gaya Hidup': 1})
 
-    st.write("***1. Cleansing***")
+#     st.write("***1. Cleansing***")
 
-    def remove_url(data_berita):
-        url = re.compile(r'https?://\S+|www\.S+')
-        return url.sub(r'', data_berita)
+#     def remove_url(data_berita):
+#         url = re.compile(r'https?://\S+|www\.S+')
+#         return url.sub(r'', data_berita)
 
-    def remove_html(data_berita):
-        html = re.compile(r'<.#?>')
-        return html.sub(r'', data_berita)
+#     def remove_html(data_berita):
+#         html = re.compile(r'<.#?>')
+#         return html.sub(r'', data_berita)
 
-    def remove_emoji(data_berita):
-        emoji_pattern = re.compile("[" 
-                                   u"\U0001F600-\U0001F64F" 
-                                   u"\U0001F300-\U0001F5FF"  
-                                   u"\U0001F680-\U0001F6FF"  
-                                   u"\U0001F1E0-\U0001F1FF" 
-                                   "]+", flags=re.UNICODE)
-        return emoji_pattern.sub(r'', data_berita)
+#     def remove_emoji(data_berita):
+#         emoji_pattern = re.compile("[" 
+#                                    u"\U0001F600-\U0001F64F" 
+#                                    u"\U0001F300-\U0001F5FF"  
+#                                    u"\U0001F680-\U0001F6FF"  
+#                                    u"\U0001F1E0-\U0001F1FF" 
+#                                    "]+", flags=re.UNICODE)
+#         return emoji_pattern.sub(r'', data_berita)
 
-    def remove_numbers(data_berita):
-        return re.sub(r'\d+', '', data_berita)
+#     def remove_numbers(data_berita):
+#         return re.sub(r'\d+', '', data_berita)
 
-    def remove_symbols(data_berita):
-        return re.sub(r'[^a-zA-Z0-9\s]', '', data_berita)
+#     def remove_symbols(data_berita):
+#         return re.sub(r'[^a-zA-Z0-9\s]', '', data_berita)
     
 
-    data['cleansing'] = data[2].apply(lambda x: remove_url(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_html(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_emoji(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_symbols(x))
-    data['cleansing'] = data['cleansing'].apply(lambda x: remove_numbers(x))
+#     data['cleansing'] = data[2].apply(lambda x: remove_url(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_html(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_emoji(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_symbols(x))
+#     data['cleansing'] = data['cleansing'].apply(lambda x: remove_numbers(x))
 
-    # st.write("CLEANSING")
+#     # st.write("CLEANSING")
 
-    st.dataframe(data)
+#     st.dataframe(data)
 
-    st.write("***2. Case folding***")
+#     st.write("***2. Case folding***")
 
-    # def case_folding(text):
-    #     return text.lower() if isinstance(text, str) else text
+#     # def case_folding(text):
+#     #     return text.lower() if isinstance(text, str) else text
 
-    def case_folding(text):
-        if isinstance(text, str):
-            lowercase_text = text.lower()
-            return lowercase_text
-        else :
-            return text
+#     def case_folding(text):
+#         if isinstance(text, str):
+#             lowercase_text = text.lower()
+#             return lowercase_text
+#         else :
+#             return text
         
-    data ['case_folding'] = data['cleansing'].apply(case_folding)
+#     data ['case_folding'] = data['cleansing'].apply(case_folding)
 
-    st.dataframe(data)
+#     st.dataframe(data)
 
-    st.write("***3. Tokenization***")
+#     st.write("***3. Tokenization***")
 
-    def tokenize(text):
-        tokens = text.split()
-        return tokens
+#     def tokenize(text):
+#         tokens = text.split()
+#         return tokens
 
-    data['tokenize'] = data['case_folding'].apply(tokenize)
+#     data['tokenize'] = data['case_folding'].apply(tokenize)
 
-    # st.write("TOKENISASI")
+#     # st.write("TOKENISASI")
 
-    st.dataframe(data)
-
-
-    st.write("***3. Stop Words***")
-
-    # Download stopwords (pastikan hanya perlu sekali saja)
-    nltk.download('stopwords')
-
-    # Mengambil daftar stopwords dalam bahasa Indonesia
-    stop_words = stopwords.words('indonesian')
-
-    # Fungsi untuk menghapus stopwords
-    def remove_stopwords(text):
-        return [word for word in text if word not in stop_words]
-
-    # Asumsi 'data' adalah dataframe, dengan kolom 'tokenize' yang berisi teks yang sudah di-tokenisasi
-    data['stopword_removal'] = data['tokenize'].apply(lambda x: ' '.join(remove_stopwords(x)))
-
-    # Menampilkan dataframe setelah penghapusan stopwords
-    st.dataframe(data['stopword_removal'])
-
-    st.write("***4. Stemming***")
-
-    factory = StemmerFactory()
-    stemmer = factory.create_stemmer()
-
-    def stemming(text):
-        return ' '.join([stemmer.stem(word) for word in text])
-
-#########
-
-    # data['stemming'] = data['stopword_removal'].apply(lambda x: ' '.join(stemming(x.split())))
+#     st.dataframe(data)
 
 
-    # # st.write("STEMING")
+#     st.write("***3. Stop Words***")
 
-    # st.dataframe(data['stemming'])
+#     # Download stopwords (pastikan hanya perlu sekali saja)
+#     nltk.download('stopwords')
 
-    url1 = "https://raw.githubusercontent.com/jennamacwe/Tic-Tac-Toe/refs/heads/main/stemming_output.csv"
-    data1 = pd.read_csv(url1, header=None)
+#     # Mengambil daftar stopwords dalam bahasa Indonesia
+#     stop_words = stopwords.words('indonesian')
 
-    # Menampilkan data
-    st.dataframe(data1[10])
+#     # Fungsi untuk menghapus stopwords
+#     def remove_stopwords(text):
+#         return [word for word in text if word not in stop_words]
 
-############
+#     # Asumsi 'data' adalah dataframe, dengan kolom 'tokenize' yang berisi teks yang sudah di-tokenisasi
+#     data['stopword_removal'] = data['tokenize'].apply(lambda x: ' '.join(remove_stopwords(x)))
 
-    st.write("### Split Data")
+#     # Menampilkan dataframe setelah penghapusan stopwords
+#     st.dataframe(data['stopword_removal'])
 
-    x = data1[10].values
-    y = data1[5].values
+#     st.write("***4. Stemming***")
 
-    Xtrain, Xtest,Ytrain,Ytest = train_test_split(x,y,test_size=0.2,random_state=2)
+#     factory = StemmerFactory()
+#     stemmer = factory.create_stemmer()
 
-    st.write("Xtrain")
-    st.dataframe(Xtrain)
+#     def stemming(text):
+#         return ' '.join([stemmer.stem(word) for word in text])
 
-    st.write("Xtest")
-    st.dataframe(Xtest)
+# #########
 
-    st.write("Ytrain")
-    st.dataframe(Ytrain)
+#     # data['stemming'] = data['stopword_removal'].apply(lambda x: ' '.join(stemming(x.split())))
 
-    st.write("Ytest")
-    st.dataframe(Ytest)
 
-    ############ TFIDF
+#     # # st.write("STEMING")
 
-    vect = TfidfVectorizer()
-    X = vect.fit_transform(Xtrain)
+#     # st.dataframe(data['stemming'])
 
-    X_array = vect.transform(Xtrain)
+#     url1 = "https://raw.githubusercontent.com/jennamacwe/Tic-Tac-Toe/refs/heads/main/stemming_output.csv"
+#     data1 = pd.read_csv(url1, header=None)
 
-    X_array.toarray()
+#     # Menampilkan data
+#     st.dataframe(data1[10])
 
-    with open('tfidf_vectorizer.pkl', 'wb') as f:
-        pickle.dump(vect, f)
+# ############
 
-with Input:
-    # st.write("### Logistic Regression")
+#     st.write("### Split Data")
 
-    ############# LR
+#     x = data1[10].values
+#     y = data1[5].values
 
-    # Membuat model Logistic Regression
-    model = LogisticRegression()
+#     Xtrain, Xtest,Ytrain,Ytest = train_test_split(x,y,test_size=0.2,random_state=2)
 
-    # Melatih model dengan data pelatihan
-    model.fit(X, Ytrain)
+#     st.write("Xtrain")
+#     st.dataframe(Xtrain)
 
-    # Meyimpan model
-    with open('logistic_model.pkl', 'wb') as f:
-        pickle.dump(model, f)
+#     st.write("Xtest")
+#     st.dataframe(Xtest)
 
-    def load_model_and_vectorizer():
-        with open('logistic_model.pkl', 'rb') as model_file:
-            model = pickle.load(model_file)
+#     st.write("Ytrain")
+#     st.dataframe(Ytrain)
 
-        with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
-            vectorizer = pickle.load(vectorizer_file)
-        return model, vectorizer
+#     st.write("Ytest")
+#     st.dataframe(Ytest)
 
-    def predict_category(berita_input, model, vectorizer):
-        def preprocessing(berita):
-            berita = remove_url(berita)
-            berita = remove_html(berita)
-            berita = remove_emoji(berita)
-            berita = remove_symbols(berita)
-            berita = remove_numbers(berita)
-            berita = berita.lower()
-            tokens = berita.split()
-            tokens = [word for word in tokens if word not in stop_words]
-            stemming_result = [stemmer.stem(word) for word in tokens]
-            return ' '.join(stemming_result)
+#     ############ TFIDF
 
-        preprocessed_berita = preprocessing(berita_input)
-        berita_vectorized = vectorizer.transform([preprocessed_berita])
-        prediction = model.predict(berita_vectorized)
-        return 'Politik' if prediction == 0 else 'Gaya Hidup'
+#     vect = TfidfVectorizer()
+#     X = vect.fit_transform(Xtrain)
 
-    st.title("Prediksi Kategori Berita")
-    user_input = st.text_area("Isi Berita", height=200)
+#     X_array = vect.transform(Xtrain)
 
-    if st.button("Prediksi"):
-        if user_input:
-            model, vectorizer = load_model_and_vectorizer()
-            predicted_category = predict_category(user_input, model, vectorizer)
-            st.write(f"**Kategori berita:** {predicted_category}")
-        else:
-            st.write("Harap masukkan isi berita.")
+#     X_array.toarray()
+
+#     with open('tfidf_vectorizer.pkl', 'wb') as f:
+#         pickle.dump(vect, f)
+
+# with Input:
+#     # st.write("### Logistic Regression")
+
+#     ############# LR
+
+#     # Membuat model Logistic Regression
+#     model = LogisticRegression()
+
+#     # Melatih model dengan data pelatihan
+#     model.fit(X, Ytrain)
+
+#     # Meyimpan model
+#     with open('logistic_model.pkl', 'wb') as f:
+#         pickle.dump(model, f)
+
+#     def load_model_and_vectorizer():
+#         with open('logistic_model.pkl', 'rb') as model_file:
+#             model = pickle.load(model_file)
+
+#         with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+#             vectorizer = pickle.load(vectorizer_file)
+#         return model, vectorizer
+
+#     def predict_category(berita_input, model, vectorizer):
+#         def preprocessing(berita):
+#             berita = remove_url(berita)
+#             berita = remove_html(berita)
+#             berita = remove_emoji(berita)
+#             berita = remove_symbols(berita)
+#             berita = remove_numbers(berita)
+#             berita = berita.lower()
+#             tokens = berita.split()
+#             tokens = [word for word in tokens if word not in stop_words]
+#             stemming_result = [stemmer.stem(word) for word in tokens]
+#             return ' '.join(stemming_result)
+
+#         preprocessed_berita = preprocessing(berita_input)
+#         berita_vectorized = vectorizer.transform([preprocessed_berita])
+#         prediction = model.predict(berita_vectorized)
+#         return 'Politik' if prediction == 0 else 'Gaya Hidup'
+
+#     st.title("Prediksi Kategori Berita")
+#     user_input = st.text_area("Isi Berita", height=200)
+
+#     if st.button("Prediksi"):
+#         if user_input:
+#             model, vectorizer = load_model_and_vectorizer()
+#             predicted_category = predict_category(user_input, model, vectorizer)
+#             st.write(f"**Kategori berita:** {predicted_category}")
+#         else:
+#             st.write("Harap masukkan isi berita.")
 
 
 ################################################################################################
